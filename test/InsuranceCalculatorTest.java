77d0d41f0e14639a83cce13af288800d69673515
Jawad Al-Mamoon <jaalmamoon@bsu.edu>
2020-02-13 11:26:34 -0500

    Added Tests

--- test/InsuranceCalculatorTest.java
+++ test/InsuranceCalculatorTest.java
@@ -4,4 +4,29 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class InsuranceCalculatorTest {
 
+    private double insuranceFor(double income) {
+        return new InsuranceCalculator().calculateInsurance(income);
+    }
+
+    @Test
+    public void low() {
+        assertEquals(1825, insuranceFor(5000), 0.01);
+    }
+
+    @Test
+    public void medium() {
+        assertEquals(38600, insuranceFor(25000), 0.01);
+    }
+
+    @Test
+    public void high() {
+        assertEquals(78500, insuranceFor(50000), 0.01);
+    }
+
+    @Test
+    public void veryHigh() {
+        assertEquals(106400, insuranceFor(100_000), 0.01);
+    }
+
+
 }
